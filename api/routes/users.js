ec56c5ed8861c9fcf6737aed2bd5a6424f1c79b6
Ariel Elazarov <ariel.elazarov@yahoo.com>
2020-12-28 16:51:04 -0500

    bcrypt passwords

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const User = require("../models/UserSchema");
 var jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
 
 app.get("/users", (req, res) => {
@@ -16,30 +17,35 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
-    User.findOne({ email: req.body.email })
-        .then((user) => {
-            if (user) return res.status(400).send("Existing email");
-            else {
-                return User.create({
-                    email: req.body.email,
-                    password: req.body.password,
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName
-                }).then((user) => {
-                    var token = jwt.sign({ id: user._id }, 'shhhh')
-                    res.status(200).json({ 
-                        token, 
-                        user: {
-                            email: user.email,
-                            userType: user.userType
-                        } 
-                    })
+
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        User.findOne({ email: req.body.email })
+                .then((user) => {
+                    if (user) return res.status(400).send("Existing email");
+                    else {
+                        return User.create({
+                            email: req.body.email,
+                            password: hash,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName
+                        }).then((user) => {
+                            var token = jwt.sign({ id: user._id }, 'shhhh')
+                            res.status(200).json({ 
+                                token, 
+                                user: {
+                                    email: user.email,
+                                    userType: user.userType
+                                } 
+                            })
+                        })
+                    }
+                })   
+                .catch((err) => {
+                    res.status(500).send("error")
                 })
-            }
-        })   
-        .catch((err) => {
-            res.status(500).send("error")
-        })
+    });
+
+    
         
 })
 
@@ -47,18 +53,23 @@ app.post("/login", (req, res) => {
     User.findOne({ email: req.body.email })
         .then((user) => {
             if (!user) res.status(403).send("Invalid credentials");
-            else if (user.password === req.body.password) {
-                var token = jwt.sign({ id: user._id }, 'shhhh')
-                res.status(200).json({ 
-                    token, 
-                    user: {
-                        email: user.email,
-                        userType: user.userType
-                    } 
-                })
-                
-            }
-            else {
+            else if (user) {
+                bcrypt.compare(req.body.password, user.password, function (err, result) {
+                    
+                    if(err) res.status(500).send("Invalid");
+                    if(result) {
+                        var token = jwt.sign({ id: user._id }, 'shhhh')
+                        res.status(200).json({ 
+                            token, 
+                            user: {
+                                email: user.email,
+                                userType: user.userType
+                            } 
+                        })
+                    } else res.status(403).send("invalid");
+                    
+                })  
+            } else {
                 res.status(403).send("Invalid credentials");
             }
         })
